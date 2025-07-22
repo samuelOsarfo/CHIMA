a9ae7b83a2b7e908c3c911b519394c3401d28655
samuelOsarfo <fsosarfo@gmail.com>
2025-07-22 09:44:45 -0500

    added package doc

---
+++ R/CHIMA-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+#' CHIMA: Package for High Dimensional Mediation Analysis
+#'
+#' Brief overview of what the package does.
+#'
+#' @section Vignettes:
+#' See \code{vignette("getting-started", package = "CHIMA")} for a walkthrough.
+#'
+#' @docType package
+#' @name CHIMA
+#' @aliases CHIMA-package
+"_PACKAGE"
+
